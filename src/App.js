5260e8903fdd5d9e7fdab3cde2d38ae98066d77e
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-05-01 16:02:28 +0100

    Added functionality to filter by options

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  //State
   const [inputText, setInputText] = useState('');
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  //Use Effect
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  //Functions
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
 
   return (
     <div className='App'>
       <header>
-        <h1>Todo List</h1>
+        <h1>Shopping List</h1>
       </header>
       <Form
         inputText={inputText}
@@ -20,7 +42,11 @@ const App = () => {
         setInputText={setInputText}
         setStatus={setStatus}
       />
-      <TodoList setTodos={setTodos} todos={todos} />
+      <TodoList
+        setTodos={setTodos}
+        todos={todos}
+        filteredTodos={filteredTodos}
+      />
     </div>
   );
 };
