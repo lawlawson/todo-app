5bc68db63d78c06a4894e3c7008b41b7baafd5e0
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-05-01 17:31:17 +0100

    Added local storage

--- src/App.js
+++ src/App.js
@@ -10,23 +10,43 @@ const App = () => {
   const [status, setStatus] = useState('all');
   const [filteredTodos, setFilteredTodos] = useState([]);
 
+  //will run only once
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
   //Use Effect
   useEffect(() => {
+    //Functions
+
+    const filterHandler = () => {
+      switch (status) {
+        case 'completed':
+          setFilteredTodos(todos.filter((todo) => todo.completed === true));
+          break;
+        case 'uncompleted':
+          setFilteredTodos(todos.filter((todo) => todo.completed === false));
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+    };
     filterHandler();
+    saveLocalTodos();
   }, [todos, status]);
 
-  //Functions
-  const filterHandler = () => {
-    switch (status) {
-      case 'completed':
-        setFilteredTodos(todos.filter((todo) => todo.completed === true));
-        break;
-      case 'uncompleted':
-        setFilteredTodos(todos.filter((todo) => todo.completed === false));
-        break;
-      default:
-        setFilteredTodos(todos);
-        break;
+  //Save to local
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  };
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem('todos'));
+      setTodos(todoLocal);
     }
   };
 
